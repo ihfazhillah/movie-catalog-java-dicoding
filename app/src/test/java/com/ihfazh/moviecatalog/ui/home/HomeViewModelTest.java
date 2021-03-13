3eec1de8b86170750714efee7da7fb0719586d69
ihfazhillah <mihfazhillah@gmail.com>
2021-03-13 19:04:53 +0700

    remove unused imports

--- app/src/test/java/com/ihfazh/moviecatalog/ui/home/HomeViewModelTest.java
+++ app/src/test/java/com/ihfazh/moviecatalog/ui/home/HomeViewModelTest.java
@@ -19,7 +19,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HomeViewModelTest {
--- app/src/test/java/com/ihfazh/moviecatalog/ui/movie/DetailMovieViewModelTest.java
+++ app/src/test/java/com/ihfazh/moviecatalog/ui/movie/DetailMovieViewModelTest.java
@@ -18,7 +18,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DetailMovieViewModelTest {
